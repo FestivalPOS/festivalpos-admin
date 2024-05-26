99ec5e09397f7ed14a55754b5439d6d2bc62e4c3
screeper <sebastian.wehrli@gmail.com>
2024-05-26 13:22:40 +0200

    small bug fix

--- src/helpers/currency.filter.ts
+++ src/helpers/currency.filter.ts
@@ -1,4 +1,4 @@
-export function currency(value) {
+export function currency(value: number) {
   if (!value) return ''
   return new Intl.NumberFormat('de-CH', { style: 'currency', currency: 'CHF' }).format(value)
 }
