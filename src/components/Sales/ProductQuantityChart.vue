af699d6efc3a334717efdd0439b4df3a5c2fa031
screeper <sebastian.wehrli@gmail.com>
2024-06-05 23:29:50 +0200

    bar colors

--- src/components/Sales/ProductQuantityChart.vue
+++ src/components/Sales/ProductQuantityChart.vue
@@ -47,6 +47,26 @@ export default {
   }),
   async mounted() {
     this.loaded = false
+    const colors = {
+      background: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      border: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ]
+    }
 
     try {
       const productSalesResponse = await axios.get(
@@ -71,11 +91,12 @@ export default {
       })
 
       const datasets = Object.keys(products).map((product, index) => {
+        const colorIndex = index % colors.background.length
         return {
           label: product,
           data: vendors.map((vendor) => products[product][vendor] || 0),
-          backgroundColor: `rgba(${index * 50}, ${index * 50}, ${index * 50}, 0.2)`,
-          borderColor: `rgba(${index * 50}, ${index * 50}, ${index * 50}, 1)`,
+          backgroundColor: colors.background[colorIndex],
+          borderColor: colors.border[colorIndex],
           borderWidth: 1
         }
       })
